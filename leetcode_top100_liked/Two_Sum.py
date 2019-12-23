aadfb5367f74fb74656f04f7e603f3575f801f72
yjiao1213 <36117600+yjiao1213@users.noreply.github.com>
2019-12-24 00:21:52 +0800

    Update Two_Sum.py

--- leetcode_top100_liked/Two_Sum.py
+++ leetcode_top100_liked/Two_Sum.py
@@ -13,3 +13,12 @@ class Solution(object):
                 return dic[target-num], i
             dic[num] = i
         return None 
+    
+    
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if target-nums[i] in nums[i+1:]:
+                return [i, nums[i+1:].index(target-nums[i])+i+1]
+        return []
+        
